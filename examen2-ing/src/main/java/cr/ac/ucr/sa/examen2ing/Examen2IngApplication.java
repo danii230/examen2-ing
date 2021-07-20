97ec90f556a1a84330199ddbcf3cc5082f9b7017
danii230 <daniiela.p182@gmail.com>
2021-07-20 15:50:08 -0600

    Commit

---
+++ examen2-ing/src/main/java/cr/ac/ucr/sa/examen2ing/Examen2IngApplication.java
@@ -0,0 +1,13 @@
+package cr.ac.ucr.sa.examen2ing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Examen2IngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Examen2IngApplication.class, args);
+	}
+
+}
